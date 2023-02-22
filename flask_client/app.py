9b36418975a0aa20f78343c1adef190208e92f34
Jay Jagtap <jayjagtap@Jays-MacBook-Pro.local>
2023-02-21 23:22:00 -0800

    app.run()

--- flask_client/app.py
+++ flask_client/app.py
@@ -59,4 +59,5 @@ def upload_file():
         context.term()
     
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=9000)
+    # app.run(host="0.0.0.0", debug=True, port=9000) # Test on PC
+    app.run()
